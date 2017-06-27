1ea0f704631f235090596d50790c24dfae7689b6
Matthew (Barry) Sampson <mbsampson89@gmail.com>
2017-06-27 16:19:19 -0700

    Skill Section Update
    -Major overhaul of the skill section
    -Added js and moving parts to accurately represent my current skillset

--- main.js
+++ main.js
@@ -1,20 +1,59 @@
-function mySkills() {
-	"use strict";
-	
-	var webSkills = ["javascript", "bootstrap", "css", "html", "dreamweaver"],
-	    learning = ["react.js", "angular.js", "full stack development"];
+var skillsBtn = document.querySelector("#skills_btn"),
+	skillIcons = document.querySelectorAll(".skill_icon"),
+	skillText = document.querySelectorAll(".skill_text");
 
-	console.log("----------Matthew Sampson's Skills----------" + '\n');
+init();
 
-	for (var i = 0; i < learning.length; i++) {
-		webSkills.push(learning[i]);
-	}
-
-	for (var i = 0; i < webSkills.length; i++) {
-		if (webSkills.length - learning.length === i) {
-			console.log(">>>>>>>>>>Upcoming Skills<<<<<<<<<<" + '\n' + webSkills[i]);
+function moveSkillIcons(){
+	for(var i = 0; i < skillIcons.length; i++){
+		if(i % 2 === 0){
+			skillIcons[i].classList.add("move_left");
+			
 		} else {
-	  		console.log(webSkills[i] + '\n');
+			skillIcons[i].classList.add("move_right");
 		}
 	}
+}
+
+function initializeSkillIcons(){
+	for(var i = 0; i < skillIcons.length; i++){
+		// Alternates the spacecraft's facing direction
+		if(i % 2 === 0){
+			skillIcons[i].style.backgroundImage = "url('images/spacecraft_left.png')";
+		}
+		skillIcons[i].style.display = "none";
+	}
+	
+	styleIconText();
+}
+
+function styleIconText(){
+	// Assign particular CSS properties to each skill icon to center its content text.
+	// Css
+	skillText[0].style.padding = "17px 30px";
+	// Bootstrap
+	// HTML
+	skillText[2].style.padding = "17px 30px";
+	// JavaScript
+	// Github
+	skillText[4].style.padding = "17px 20px";
+	// Dreamweaver
+	skillText[5].style.fontSize = "0.8em";
+	skillText[5].style.padding = "19px 0 0 20px";
+}
+
+function showSkillsIcons(){
+	for(var i = 0; i < skillIcons.length; i++){
+		skillIcons[i].style.display = "block";
+	}
+}
+
+function init(){
+	initializeSkillIcons();
+	skillsBtn.addEventListener("click", function(){
+		// Populate field
+		showSkillsIcons();
+		// Move icons
+		setTimeout(moveSkillIcons, 1000);
+	});
 }
